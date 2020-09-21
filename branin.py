44db49407ea604d9d3661bcf6bc8f246d092634c
Sam Hamedi Rad <hamedirad89@gmail.com>
2020-09-20 21:37:28 -0700

    Update branin.py

--- branin.py
+++ branin.py
@@ -1,18 +1,20 @@
+#This code was mainly adopted from Spearmint paper.
 import numpy as np
 import sys
 import math
 import time
 import numpy as np
 import random
-
+#Gaussian process
 def gaussian(x, mu, sig):
     return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
+# GMM
 def Oracle(x):
   return 2*gaussian(x[0],12,10)+3*gaussian(x[1],19,10)+4*gaussian(x[2],17,7)
+#braning was the name of the function originally used in the example
 def branin(x):
-  # time.sleep(1000000)
   return -Oracle(x)+4*random.random()-2
-# Write a function like this called 'main'
+#This is the main function and was used in the example.
 def main(job_id, params):
   print 'Anything printed here will end up in the output directory for job #:', str(job_id)
   print params
