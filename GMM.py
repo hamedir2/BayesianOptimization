2b768877512dfcd6fd9de3930b6c21c61429a2f1
Sam Hamedi Rad <hamedirad89@gmail.com>
2024-08-15 16:31:23 -0700

    Update and rename branin.py to GMM.py

--- GMM.py
+++ GMM.py
@@ -11,11 +11,10 @@ def gaussian(x, mu, sig):
 # GMM
 def Oracle(x):
   return 2*gaussian(x[0],12,10)+3*gaussian(x[1],19,10)+4*gaussian(x[2],17,7)
-#braning was the name of the function originally used in the example
-def branin(x):
+def GMM(x):
   return -Oracle(x)+4*random.random()-2
 #This is the main function and was used in the example.
 def main(job_id, params):
   print 'Anything printed here will end up in the output directory for job #:', str(job_id)
   print params
-  return branin(params['X'])
+  return GMM(params['X'])
